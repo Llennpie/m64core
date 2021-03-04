c1015ce84b25e7d2258fc57fb85e25120443ed5c
Llennpie <44985633+Llennpie@users.noreply.github.com>
2021-03-04 17:06:20 -0500

    Add Fusion style
    GUI will now use Qt5's "Fusion" style on all platforms for more consistency. A dark version has also been added for Linux platforms.

--- mupen64plus-gui/main.cpp
+++ mupen64plus-gui/main.cpp
@@ -1,6 +1,10 @@
 #include "mainwindow.h"
 #include <QApplication>
+#include <QStyleFactory>
+#include <QPalette>
 #include <QCommandLineParser>
+#include <QString>
+#include <QSettings>
 
 MainWindow *w = nullptr;
 int main(int argc, char *argv[])
@@ -8,6 +12,32 @@ int main(int argc, char *argv[])
     srand (time(NULL));
 
     QApplication a(argc, argv);
+
+    a.setStyle(QStyleFactory::create("Fusion"));
+
+    #ifdef Q_OS_LINUX
+        // Enable dark mode if user is on Linux (looks kind of ugly on Windows 10).
+        // Feel free to change this for dark mode on other platforms.
+        QPalette darkPalette;
+        darkPalette.setColor(QPalette::Window, QColor(53,53,53));
+        darkPalette.setColor(QPalette::WindowText, Qt::white);
+        darkPalette.setColor(QPalette::Base, QColor(25,25,25));
+        darkPalette.setColor(QPalette::AlternateBase, QColor(53,53,53));
+        darkPalette.setColor(QPalette::ToolTipBase, Qt::white);
+        darkPalette.setColor(QPalette::ToolTipText, Qt::white);
+        darkPalette.setColor(QPalette::Text, Qt::white);
+        darkPalette.setColor(QPalette::Button, QColor(53,53,53));
+        darkPalette.setColor(QPalette::ButtonText, Qt::white);
+        darkPalette.setColor(QPalette::BrightText, Qt::red);
+        darkPalette.setColor(QPalette::Link, QColor(42, 130, 218));
+
+        darkPalette.setColor(QPalette::Highlight, QColor(42, 130, 218));
+        darkPalette.setColor(QPalette::HighlightedText, Qt::black);
+    
+        a.setPalette(darkPalette);
+
+        a.setStyleSheet("QToolTip { color: #ffffff; background-color: #2a82da; border: 1px solid white; }");
+    #endif
 
     QCoreApplication::setApplicationName("M64Core");
 
